8fffa9f551ea9922e2216f67b4ed28db7e63545c
daniel <danielkoopmans@live.nl>
2023-07-25 12:14:41 +0200

    (feat): add embeded contact struct

--- 2.structs/main.go
+++ 2.structs/main.go
@@ -2,17 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{firstName: "alex", lastName: "anderson"}
+	alex := person{
+		firstName: "alex",
+		lastName:  "anderson",
+		contact: contactInfo{
+			email:   "alex.anders@gmail.com",
+			zipCode: 8080,
+		},
+	}
 
-	fmt.Printf("Person: %+v", alex)
-
-	alex.lastName = "apple"
-
-	fmt.Printf("Person: %+v", alex)
+	fmt.Printf("%+v", alex)
 }
