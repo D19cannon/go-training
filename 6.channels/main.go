640c65972131d8046be28caa52700a2268aaec58
daniel <danielkoopmans@live.nl>
2023-08-08 10:03:40 +0200

    (feat): setup channels with go routines

--- 6.channels/main.go
+++ 6.channels/main.go
@@ -13,17 +13,25 @@ func main() {
 		"https://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go pingUrl(link)
+		go pingUrl(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func pingUrl(url string) {
+func pingUrl(url string, c chan string) {
 	_, err := http.Get(url)
 	if err != nil {
 		fmt.Println(url, "might be down")
+		c <- "might be down"
 		return
 	}
 
 	fmt.Println(url, "is up")
+	c <- "is up"
 }
